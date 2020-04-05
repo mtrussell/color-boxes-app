6c467df51eeb56e9cf1178390fc833f5d256d66f
Matt <smtrussell@gmail.com>
2020-04-05 13:07:33 -0500

    Updates

---
+++ precache-manifest.3196cc179370cda6e339202107f103a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2a1dd4d6bfc1b7709f2b9c95d149fe",
+    "url": "/color-boxes-app/index.html"
+  },
+  {
+    "revision": "d98b4600e7c89d702955",
+    "url": "/color-boxes-app/static/css/main.903b1d8a.chunk.css"
+  },
+  {
+    "revision": "ad893f85c9534d6a1ffa",
+    "url": "/color-boxes-app/static/js/2.9eea1258.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-boxes-app/static/js/2.9eea1258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98b4600e7c89d702955",
+    "url": "/color-boxes-app/static/js/main.5f05694a.chunk.js"
+  },
+  {
+    "revision": "4cabf6981c0a4239ae50",
+    "url": "/color-boxes-app/static/js/runtime-main.d40aa143.js"
+  }
+]);
