c5a9ad5bcd967484f5980ad9d327b7707349ce89
Matt <smtrussell@gmail.com>
2020-04-04 19:25:27 -0500

    color boxes

---
+++ src/BoxContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import './BoxContainer.css'
+
+class BoxContainer extends Component {
+  static defaultProps = {
+    numBoxes: 18,
+    allColors: ['magenta', 'pink', 'plum', 'violet', 'deeppink', 'turquoise', 'teal', 'steelblue', 'skyblue']
+  }
+
+  render() {
+    const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+      <Box colors={this.props.allColors} />
+    )); 
+
+    return(
+      <div className='BoxContainer'>
+        {boxes}
+      </div>
+    );
+  }
+}
+
+export default BoxContainer;
